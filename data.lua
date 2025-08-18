c48fa156d131dc7c21e44c48efc5da3bd91e9ab6
sg-mcgee <garton@ou.edu>
2025-08-18 13:04:34 -0500

    Fixing remnants png issue

--- data.lua
+++ data.lua
@@ -1,9 +1,9 @@
 --data.lua
 
-local sprite_idle_filename = "__rosc-and-bart-bots__/graphics/entity/roscoe_idle2_transparent_resized.png"
+local sprite_idle_filename = "__rosc-and-bart-bots__/graphics/entity/logistic-robot/roscoe_idle2_transparent_resized.png"
 local sprite_with_cargo_filename = "__rosc-and-bart-bots__/graphics/entity/graphics/entity/logistic-robot/roscoe_cargo_transparent_resized.png"
-local sprite_travel_filename = "__rosc-and-bart-bots__/graphics/entity/roscoe_load_transparent_resized.png"
-local sprite_remnant_filename = "__rosc-and-bart-bots__/graphics/entity/roscoe_tired_transparent_resized.png"
+local sprite_travel_filename = "__rosc-and-bart-bots__/graphics/entity/logistic-robot/roscoe_load_transparent_resized.png"
+local sprite_remnant_filename = "__rosc-and-bart-bots__/graphics/entity/logistic-robot/remnants/roscoe_tired_transparent_resized.png"
 
 data.raw["logistic-robot"]["logistic-robot"].idle.filename = sprite_idle_filename
 data.raw["logistic-robot"]["logistic-robot"].idle_with_cargo.filename = sprite_with_cargo_filename
