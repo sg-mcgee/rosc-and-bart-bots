155bfeadae0252c07a066e835a678363290e8e14
sg-mcgee <garton@ou.edu>
2025-08-18 12:33:31 -0500

    Substance added

---
+++ data.lua
@@ -0,0 +1,5 @@
+--data.lua
+
+local sprite_idle_filename = "__rosc-and-bart-bots__/graphics/entity/roscoe_idle2_transparent_resized"
+local sprite_travel_filename = "__rosc-and-bart-bots__/graphics/entity/roscoe_load_transparent_resized"
+local sprite_remnant_filename = "__rosc-and-bart-bots__/graphics/entity/roscoe_tired_transparent_resized"
